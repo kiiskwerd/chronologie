03a528c7a7fa06dd42c80667ab63c303ecf1a24d
Karen Cabingatan <karen.cyantis@gmail.com>
2021-12-02 01:26:08 +0800

    setup routes

---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Landing } from "./Landing"
+import { Gallery } from './Gallery'
+
+export class Home extends React.Component {
+    render() {
+        return (
+            <>
+                <Router>
+                    <Routes>
+                        <Route exact path="/" element={<Landing/>}/>
+                        <Route path="/gallery" element={<Gallery/>}/>
+                    </Routes>
+                </Router>
+            </>
+        )
+    }
+}
